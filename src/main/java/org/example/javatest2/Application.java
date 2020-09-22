7a2f68ada0dd63f76f320e805d0cb207bdb61d7a
kriksorum <kriksorum@gmail.com>
2020-09-22 21:38:40 +0800

    Init

--- src/main/java/org/example/javatest2/Application.java
+++ src/main/java/org/example/javatest2/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+        System.out.println("Run");
         SpringApplication.run(Application.class, args);
     }
 }
